fa662d2fd9a62a526d7e1fde4484aafb2484ae98
jaqueline <jaquelinesa.82@gmail.com>
2021-09-18 00:13:56 -0300

    Criado logout

--- sos_animal/urls.py
+++ sos_animal/urls.py
@@ -7,7 +7,7 @@ app_name = 'sos_animal'
 urlpatterns = [
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
-    path('logout', views.logout_user),
+    path('logout/', views.logout_user),
     path('', RedirectView.as_view(url='pet/all/')),
     path('pet/all/', views.list_all_pets),
     path('pet/user/', views.list_user_pet),
